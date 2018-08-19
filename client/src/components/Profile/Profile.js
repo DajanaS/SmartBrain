5bb2187d57e285dbcc0cf3e499ef174d5551a2b5
DajanaS <dajana.sk@hotmail.com>
2018-08-19 22:30:10 +0200

    Update profile - frontend

--- client/src/components/Profile/Profile.js
+++ client/src/components/Profile/Profile.js
@@ -1,56 +1,100 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './Profile.css';
 
-const Profile = ({isProfileOpen, toggleModal}) => {
-    return (
-        <div className="profile-modal">
-            <article className="br3 ba b--black-10 mv4 w-100 w-50-m w-25-l mw6 shadow-5 center bg-white">
-                <main className="pa4 black-80 w-80">
-                    <img
-                        src="https://avatars1.githubusercontent.com/u/8987819"
-                        className="h3 w3 dib" alt="avatar"/>
-                    <h1>Dajana Stojchevska</h1>
-                    <h4>Images submitted: 5</h4>
-                    <p>Member since: January</p>
-                    <hr/>
-                    <label className="mt2 fw6" htmlFor="username">Name:</label>
-                    <input
-                        className="pa2 ba w-100"
-                        placeholder="Dajana"
-                        type="text"
-                        name="username"
-                        id="name"
-                    />
-                    <label className="mt2 fw6" htmlFor="userAge">Age:</label>
-                    <input
-                        className="pa2 ba w-100"
-                        placeholder="22"
-                        type="text"
-                        name="userAge"
-                        id="age"
-                    />
-                    <label className="mt2 fw6" htmlFor="userPet">Pet:</label>
-                    <input
-                        className="pa2 ba w-100"
-                        placeholder="Cat"
-                        type="text"
-                        name="userPet"
-                        id="pet"
-                    />
-                    <div className="mt4" style={{display: 'flex', justifyContent: 'space-evenly'}}>
-                        <button className="b pa2 grow pointer hover-white w-40 bg-green b--black-20">
-                            Save
-                        </button>
-                        <button className="b pa2 grow pointer hover-white w-40 bg-light-red b--black-20"
-                                onClick={toggleModal}>
-                            Cancel
-                        </button>
-                    </div>
-                </main>
-                <div className="modal-close" onClick={toggleModal}>&times;</div>
-            </article>
-        </div>
-    );
-};
+class Profile extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            name: this.props.user.name,
+            age: this.props.user.age,
+            pet: this.props.user.pet
+        }
+    }
+
+    onFormChange = (event) => {
+        switch (event.target.name) {
+            case 'username':
+                this.setState({name: event.target.value});
+                break;
+            case 'userAge':
+                this.setState({age: event.target.value});
+                break;
+            case 'userPet':
+                this.setState({pet: event.target.value});
+                break;
+            default:
+                return;
+        }
+    };
+
+    onProfileUpdate = (data) => {
+        fetch(`http://localhost:3000/profile/${this.props.user.id}`, {
+            method: 'post',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({formInput: data})
+        }).then(resp => {
+            this.props.toggleModal();
+            this.props.loadUser({...this.props.user, ...data});
+        }).catch(console.log);
+    };
+
+    render() {
+        const {user} = this.props;
+        const {name, age, pet} = this.state;
+        return (
+            <div className="profile-modal">
+                <article className="br3 ba b--black-10 mv4 w-100 w-50-m w-25-l mw6 shadow-5 center bg-white">
+                    <main className="pa4 black-80 w-80">
+                        <img
+                            src="https://avatars1.githubusercontent.com/u/8987819"
+                            className="h3 w3 dib" alt="avatar"/>
+                        <h1>{this.state.name}</h1>
+                        <h4>{`Images submitted: ${user.entries}`}</h4>
+                        <p>{`Member since: ${new Date(user.joined).toLocaleDateString()}`}</p>
+                        <hr/>
+                        <label className="mt2 fw6" htmlFor="username">Name:</label>
+                        <input
+                            onChange={this.onFormChange}
+                            className="pa2 ba w-100"
+                            placeholder={user.name}
+                            type="text"
+                            name="username"
+                            id="name"
+                        />
+                        <label className="mt2 fw6" htmlFor="userAge">Age:</label>
+                        <input
+                            onChange={this.onFormChange}
+                            className="pa2 ba w-100"
+                            placeholder={user.age}
+                            type="text"
+                            name="userAge"
+                            id="age"
+                        />
+                        <label className="mt2 fw6" htmlFor="userPet">Pet:</label>
+                        <input
+                            onChange={this.onFormChange}
+                            className="pa2 ba w-100"
+                            placeholder={user.pet}
+                            type="text"
+                            name="userPet"
+                            id="pet"
+                        />
+                        <div className="mt4" style={{display: 'flex', justifyContent: 'space-evenly'}}>
+                            <button onClick={() => this.onProfileUpdate({name, age, pet})}
+                                    className="b pa2 grow pointer hover-white w-40 bg-green b--black-20">
+                                Save
+                            </button>
+                            <button className="b pa2 grow pointer hover-white w-40 bg-light-red b--black-20"
+                                    onClick={this.props.toggleModal}>
+                                Cancel
+                            </button>
+                        </div>
+                    </main>
+                    <div className="modal-close" onClick={this.props.toggleModal}>&times;</div>
+                </article>
+            </div>
+        );
+    };
+}
 
 export default Profile;

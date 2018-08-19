b80c1bac32eedcf182623f6ef7840a65c5b566ec
DajanaS <dajana.sk@hotmail.com>
2018-08-19 02:05:19 +0200

    Add profile dropdown menu and sign out functionality

--- client/src/components/Profile/ProfileIcon.js
+++ client/src/components/Profile/ProfileIcon.js
@@ -1,19 +1,45 @@
 import React, {Component} from 'react';
+import {Dropdown, DropdownToggle, DropdownMenu, DropdownItem} from 'reactstrap';
 
 class ProfileIcon extends Component {
     constructor(props) {
         super(props);
+
+        this.toggle = this.toggle.bind(this);
         this.state = {
             dropdownOpen: false
         }
     }
 
+    toggle = () => {
+        this.setState(prevState => ({
+            dropdownOpen: !prevState.dropdownOpen
+        }));
+    };
+
     render() {
         return (
             <div className="pa4 tc">
-                <img
-                    src="https://avatars1.githubusercontent.com/u/8987819"
-                    className="br4 h3 w3 dib" alt="avatar"/>
+                <Dropdown isOpen={this.state.dropdownOpen} toggle={this.toggle} className="pointer">
+                    <DropdownToggle
+                        tag="span"
+                        data-toggle="dropdown"
+                        aria-expanded={this.state.dropdownOpen}>
+                        <img
+                            src="https://avatars1.githubusercontent.com/u/8987819"
+                            className="br4 h3 w3 dib" alt="avatar"/>
+                    </DropdownToggle>
+                    <DropdownMenu
+                        right
+                        className="b--transparent shadow-5 pointer"
+                        style={{
+                            marginTop: '20px',
+                            backgroundColor: 'rgba(225, 225, 225, 0.5)'
+                        }}>
+                        <DropdownItem>My profile</DropdownItem>
+                        <DropdownItem onClick={() => this.props.onRouteChange('signout')}>Sign out</DropdownItem>
+                    </DropdownMenu>
+                </Dropdown>
             </div>
         );
     }
